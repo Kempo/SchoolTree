09398ae6ecba0047c0a8ce3534776f5a57ee888c
Aaron <forte-@mail.com>
2017-10-25 21:04:50 -0700

    security change

--- DisplayList.java
+++ DisplayList.java
@@ -47,7 +47,7 @@ public class DisplayList extends JPanel implements TreeSelectionListener, Action
     private JMenu menu;
     private static Reader reader = null;
     private Topic currentTopic;
-    private static String directory = "C:/Users/ros_adchen/Desktop/words.txt";
+    private static String directory = "directory path here";
     private static File file;
     private DefaultMutableTreeNode top = null;
 
